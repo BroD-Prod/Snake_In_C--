abaacbb122dc66f185df60d37178e78aad26e436
Brody Roth <brodyroth23@gmail.com>
2026-09-16 19:26:15 -0400

    creating snake body and changing map to be '~' and fixing height and width to be be actually mapped to height and width instead of flip flopped. refactoring print conditionals.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@ class Snake {
     private:
         Position head;
         int length = 3;
+
+    public:
+        int getLength() const{
+            return length;
+        }
     
     public:
         Snake(int startRow, int startCol){
@@ -27,8 +32,8 @@ class Snake {
 
 class Map {
     private:
-        int height = 45;
-        int width = 15;
+        int height = 15;
+        int width = 45;
 
         int snakeStartRow = height / 2;
         int snakeStartCol = width / 2;
@@ -36,33 +41,34 @@ class Map {
     public:
         void createMap(const Snake& snake){
             Position snakeHead = snake.getHeadPosition();
-            for(int col = 0; col < width; col++){
-                for(int row = 0; row < height; row++){
-                    if(row == 0){
+            for(int row = 0; row < height; row++){
+                for(int col = 0; col < width; col++){
+                    if(row == 0 || row == height - 1){
+                        std::cout << "-";
+                    }
+                     else if (row == snakeHead.row &&
+                        col == snakeHead.col) {
+                        std::cout << "<"; 
+                    }
+                    else if (row == snakeHead.row &&
+                        col <= snakeHead.col + snake.getLength() &&
+                        col > snakeHead.col) {
+                        std::cout << "o"; 
+                    }
+                    else if(col == 0 || col == width - 1){
                         std::cout << "|";
                     }
-                    else if(row == height - 1){
-                        std::cout << "|" << std::endl;
-                    }
-                    else if(col == snakeHead.col && row == snakeHead.row){
-                        std::cout << "<";
-                    }
-                    else if(col == 0){
-                        std::cout << "-";
-                    }
-                    else if(col == width - 1){
-                        std::cout << "-";
-                    }
                     else{
-                        std::cout << "x";
+                        std::cout << "~";
                     }
                 }
+                std::cout << std::endl;
             }
         }
 };
 
 int main(){
-    Snake snake(22, 7);
+    Snake snake(7, 22);
     Map snakeMap;
     snakeMap.createMap(snake);
     return 0;
