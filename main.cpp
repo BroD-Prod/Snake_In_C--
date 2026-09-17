1a2b76499211b93367c0fd0b0226715159f10300
Brody Roth <brodyroth23@gmail.com>
2026-09-17 18:15:06 -0400

    updating snake game with the modern c++ conventions. implementing movement functions and game loop. decreasing amount of magic numbers in main loop.

--- main.cpp
+++ main.cpp
@@ -5,44 +5,76 @@ struct Position{
     int col;
 };
 
+struct startPosition{
+    int snakeStartRow {7};
+    int snakeStartCol {22};
+};
+
 class Snake {
     private:
         Position head;
-        int length = 3;
+        int length {3};
+        char input;
 
     public:
         int getLength() const{
             return length;
         }
     
-    public:
         Snake(int startRow, int startCol){
             head.row = startRow;
             head.col = startCol;
         }
         
         void moveRight(){
-            head.col++;
+            ++head.col;
+        }
+        
+        void moveLeft(){
+            --head.col;
         }
 
-        Position getHeadPosition() const{
+        void moveUp(){
+            --head.row;
+        }
+
+        void moveDown(){
+            ++head.row;
+        }
+
+        void MoveSnake(char input){
+            switch(input){
+                case 'w':
+                    moveUp();
+                    break;
+                case 's':
+                    moveDown();
+                    break;
+                case 'd':
+                    moveRight();
+                    break;
+                case 'a':
+                    moveLeft();
+                    break;
+            }
+        }
+
+        Position getHeadPosition() const {
             return head;
         }
 };
 
 class Map {
     private:
-        int height = 15;
-        int width = 45;
-
-        int snakeStartRow = height / 2;
-        int snakeStartCol = width / 2;
+        const int height {15};
+        const int width {45};
 
     public:
-        void createMap(const Snake& snake){
+        void createMap(const Snake& snake){            
+            std::cout << "\033[" << 1 << ";" << 1 << "H";
             Position snakeHead = snake.getHeadPosition();
-            for(int row = 0; row < height; row++){
-                for(int col = 0; col < width; col++){
+            for(int row {0}; row < height; ++row){
+                for(int col {0}; col < width; ++col){
                     if(row == 0 || row == height - 1){
                         std::cout << "-";
                     }
@@ -68,8 +100,19 @@ class Map {
 };
 
 int main(){
-    Snake snake(7, 22);
+    startPosition start_position;
+    Snake snake(start_position.snakeStartRow, start_position.snakeStartCol);
     Map snakeMap;
     snakeMap.createMap(snake);
+    char input;
+    while (true)
+    {
+        std::cin >> input;
+        if(sizeof(input) != 1){
+            input = '\0';
+        }
+        snake.MoveSnake(input);
+        snakeMap.createMap(snake);
+    }
     return 0;
 }
